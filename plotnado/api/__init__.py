75c3ca5d6eeeef0e8879b1b6ce02a4811cdd93fb
alsmith <asmith151@outlook.com>
2024-04-11 12:06:23 +0100

    Refactor Figure class to support autoscaling of tracks in specified groups and add support for custom track classes in TrackWrapper

--- plotnado/api/__init__.py
+++ plotnado/api/__init__.py
@@ -1,4 +1,5 @@
-from . figure import TrackWrapper, Figure
+import coolbox.api as cb
+
 from .tracks import (
     BedMemory,
     BedSimple,
@@ -10,21 +11,24 @@ from .tracks import (
     MatrixCapcruncher,
     MatrixCapcruncherAverage,
     ScaleBar,
+    Autoscaler,
 )
+from .track_wrapper import TrackWrapper, MATRIX_TRACKS, BIGWIG_TRACKS, CUSTOM_TRACKS, FilelessTracks, TrackType
+from .figure import Figure
 
 
 __all__ = [
-    'TrackWrapper',
-    'Figure',
-    'BedMemory',
-    'BedSimple',
-    'BigwigFragment',
-    'BigwigFragmentCollection',
-    'BigwigFragmentCollectionOverlay',
-    'BigwigOverlay',
-    'GenomicAxis',
-    'MatrixCapcruncher',
-    'MatrixCapcruncherAverage',
-    'ScaleBar',
+    "Autoscaler",
+    "BedMemory",
+    "BedSimple",
+    "BigwigFragment",
+    "BigwigFragmentCollection",
+    "BigwigFragmentCollectionOverlay",
+    "BigwigOverlay",
+    "Figure",
+    "GenomicAxis",
+    "MatrixCapcruncher",
+    "MatrixCapcruncherAverage",
+    "ScaleBar",
+    "TrackWrapper",
 ]
-
