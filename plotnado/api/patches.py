150b1122bd1e7e4e9f2179ded0ce267a44871e01
alsmith <asmith151@outlook.com>
2024-09-06 15:45:16 +0100

    chore: Refactor plot_text_range function to improve code readability and consistency

--- plotnado/api/patches.py
+++ plotnado/api/patches.py
@@ -1,29 +1,31 @@
 import coolbox.api as cb
 
+def format_lim(lim):
+    return int(lim) if float(lim) % 1 == 0 else f"{lim:.2f}"
+
 
 def plot_text_range(self, ax, ymin, ymax, gr: cb.GenomeRange):
     ydelta = ymax - ymin
 
     # set min max
-    format_lim = lambda lim: int(lim) if float(lim) % 1 == 0 else "{:.2f}".format(lim)
     ymax_print = format_lim(ymax)
     ymin_print = format_lim(ymin)
     small_x = 0.01 * gr.length
 
-    if self.properties.get("data_range_location") == "right":
+    if self.properties.get("data_range_location", 'left') == "right":
         ax.text(
-            gr.end - small_x,
+            gr.end,
             ymax - ydelta * 0.2,
-            "[ {} ~ {} ]".format(ymin_print, ymax_print),
+            f"[{ymin_print} - {ymax_print}]",
             horizontalalignment="right",
             verticalalignment="top",
         )
     else:
         # by default show the data range
         ax.text(
-            gr.start - small_x,
+            gr.start,
             ymax - ydelta * 0.2,
-            "[ {} ~ {} ]".format(ymin_print, ymax_print),
+            f"[{ymin_print} - {ymax_print}]",
             horizontalalignment="left",
             verticalalignment="top",
         )
